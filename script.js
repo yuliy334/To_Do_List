c410f87a1e7ea4970de6ad1a23ee21131734df1f
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-07-17 18:21:17 +0300

    delete task button

--- script.js
+++ script.js
@@ -12,12 +12,16 @@ class Task_manager {
         this.load_From_localStorage();
         this.print_tasks();
     }
-    print_tasks(){
-        this.tasks.forEach((task,id) => {
-            this.print_new_task(task,id);
+    print_tasks() {
+        const tasks_body = document.getElementById("tasks_body");
+        tasks_body.innerHTML = `<ul id = "tasks_body">
+        
+    </ul>`
+        this.tasks.forEach((task, id) => {
+            this.print_new_task(task, id);
         });
     }
-    print_new_task(task,id) {
+    print_new_task(task, id) {
         const tasks_body = document.getElementById("tasks_body");
         const li = document.createElement("li");
         li.innerHTML = `${task.description}`;
@@ -29,7 +33,6 @@ class Task_manager {
     }
     localStorage_save() {
         localStorage.setItem("tasks", JSON.stringify(this.tasks));
-        this.print_new_task(this.tasks.at(-1), this.tasks.length - 1);
     }
     load_From_localStorage() {
         const tasksJ = localStorage.getItem("tasks");
@@ -44,6 +47,12 @@ class Task_manager {
         }
 
     }
+    delete_task(id) {
+        this.tasks.splice(id, 1);
+        this.localStorage_save();
+        this.print_tasks();
+
+    }
 }
 
 
@@ -52,6 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const add_task_button = document.getElementById("add_task_btn");
     const task_input = document.getElementById("task_input");
     const tasks_body = document.getElementById("tasks_body");
+    const settings_form = document.getElementById("setting_form");
 
 
 
@@ -60,6 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
         task_manage.tasks.push(new_task);
         console.log(task_manage.tasks);
         task_manage.localStorage_save();
+        task_manage.print_new_task(task_manage.tasks.at(-1), task_manage.tasks.length - 1);
 
 
         task_input.value = '';
@@ -69,12 +80,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     tasks_body.addEventListener("click", (e) => {
-        console.log("clicked", e.target);
+        const black_background = document.getElementById("black_background");
+        const hiden_id = document.getElementById("hiden_id");
+
+
         if (e.target.classList.contains('my_task')) {
             console.log("Task clicked:", e.target.dataset.index);
-            
+            settings_form.style.display = "flex";
+            black_background.style.display = "block";
+            hiden_id.textContent = e.target.dataset.index;
         }
-    });     
+    });
+
+    settings_form.addEventListener("click", (e) => {
+        const black_background = document.getElementById("black_background");
+        const hiden_id = parseInt(document.getElementById("hiden_id").textContent);
+        if (e.target.id === "delete_task_btn") {
+            console.log("delete ", hiden_id);
+            task_manage.delete_task(hiden_id);
+            settings_form.style.display = "none";
+            black_background.style.display = "none";
+
+        }
+    });
 
 
 
